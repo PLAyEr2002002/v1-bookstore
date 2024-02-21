abfd0b8908fee5dbca1c655bc63ada6cb433ad0a
PLAyEr2002002 <95416181+PLAyEr2002002@users.noreply.github.com>
2024-02-21 16:40:40 +0300

    Add book details to search results

--- script.js
+++ script.js
@@ -1,11 +1,30 @@
 // Sample data structure
 let books = {
-    "ics108": {row: 1, column: 2, shelf: 3},
-    "ics344": {row: 2, column: 3, shelf: 4},
-    "ics343": {row: 3, column: 4, shelf: 5},
-    "ics104": {row: 4, column: 5, shelf: 6},
-    "ics208": {row: 5, column: 6, shelf: 7},
-    "coe200": {row: 5, column: 6, shelf: 7},
+    "coe451": { name: "Information security : principles and practice / Mark Stamp. " , row: 1, column: 1 , shelf: 5},
+    "coe449": { name: "Computer security : principles and practice / William Stallings, Lawrie Brown ; with contributions by Mick Bauer [and] Michael Howard.", row: 1, column: 1 , shelf: 4},
+    "coe444": { name:" Top-down network design / Priscilla Oppenheimer." , row: 1, column: 1 , shelf: 3},
+    "coe436": { name: "Top-down network design / Priscilla Oppenheimer." , row: 1, column: 1 , shelf: 3},
+    "coe405": { name: "Advanced digital design with the Verilog HDL / Michael D. Ciletti" , row: 1, column: 1 , shelf: 2},
+    
+    // "coe403": { name:, row: 1, column: 2 , shelf: 6},
+    // "coe361": { name:, row: 1, column: 2 , shelf: 6},
+    // "coe353": { name: Business data communications and networking / Jerry FitzGerald, Jerry FitzGerald & Associates, Alan Dennis, Indiana University, Alexandra Durcikova, University of Arizona , row: 1, column: 2 , shelf: 5},
+    // "coe344": { name: Information security : principles and practice / Mark Stamp. , row: 1, column: 2 , shelf: 5},
+    // "coe306": { name: Information security : principles and practice / Mark Stamp. , row: 1, column: 2 , shelf: 5},
+    // "coe301": { name: Information security : principles and practice / Mark Stamp. , row: 1, column: 2 , shelf: 5},
+    // "coe233": { name: Information security : principles and practice / Mark Stamp. , row: 1, column: 2 , shelf: 5},
+    
+    // "coe451": { name: Information security : principles and practice / Mark Stamp. , row: 1, column: 1 , shelf: 5},
+    // "coe451": { name: Information security : principles and practice / Mark Stamp. , row: 1, column: 1 , shelf: 5},
+    // "coe451": { name: Information security : principles and practice / Mark Stamp. , row: 1, column: 1 , shelf: 5},
+    // "coe451": { name: Information security : principles and practice / Mark Stamp. , row: 1, column: 1 , shelf: 5},
+    // "coe451": { name: Information security : principles and practice / Mark Stamp. , row: 1, column: 1 , shelf: 5},
+    // "coe451": { name: Information security : principles and practice / Mark Stamp. , row: 1, column: 1 , shelf: 5},
+    // "coe451": { name: Information security : principles and practice / Mark Stamp. , row: 1, column: 1 , shelf: 5},
+    // "coe451": { name: Information security : principles and practice / Mark Stamp. , row: 1, column: 1 , shelf: 5},
+    // "coe451": { name: Information security : principles and practice / Mark Stamp. , row: 1, column: 1 , shelf: 5},
+    
+    
 };
 
 // Autocomplete feature
@@ -36,7 +55,11 @@ function searchBook() {
     let courseNumber = document.getElementById('courseNumber').value.toLowerCase().replace(/\s/g, '');
     let result = books[courseNumber];
     if(result) {
-        document.getElementById('result').innerText = `Row: ${result.row}, Column: ${result.column}, Shelf: ${result.shelf}`;
+        // document.getElementById('result').innerText = `Book Name: ${result.name} Row: ${result.row}, Column: ${result.column}, Shelf: ${result.shelf}`;
+        document.getElementById('bookname').innerText = `Book Name: ${result.name}`;
+        document.getElementById('row').innerText = `Row: ${result.row}`;  
+        document.getElementById('column').innerText = `Column: ${result.column}`;
+        document.getElementById('shelf').innerText = `Shelf: ${result.shelf}`;      
     } else {
         document.getElementById('result').innerText = "Book not found.";
     }
