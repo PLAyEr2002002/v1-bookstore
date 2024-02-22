28c0b53b672ac68488db2acba9e6d92694be344a
PLAyEr2002002 <95416181+PLAyEr2002002@users.noreply.github.com>
2024-02-22 13:18:05 +0300

    Add search button ID and update toast functionality

--- script.js
+++ script.js
@@ -18,16 +18,15 @@ window.addEventListener('load', () => {
     });
 });
 
-const toastTrigger = document.getElementById('liveToastBtn')
-const toastLiveExample = document.getElementById('liveToast')
+// toast
+var toastElList = [].slice.call(document.querySelectorAll('.toast'))
+var toastList = toastElList.map(function (toastEl) {
+  return new bootstrap.Toast(toastEl)
+})
 
-if (toastTrigger) {
-  const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
-  toastTrigger.addEventListener('click', () => {
-    toastBootstrap.show()
-  })
-}
-
+document.getElementById('search-button').addEventListener('click', function () {
+  toastList.forEach(toast => toast.show());
+});
 
 let books = {
     // column 1
